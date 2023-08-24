a09b690c147503b7d6b3c10386bff4f0670c0c71
jongjaepark <pjj186@naver.com>
2023-08-24 22:08:09 +0900

    Project - Omedetou

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+`;
+
+const InputContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #7bed9f;
+`;
+
+const NameInputBox = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const NameQuestionText = styled.span`
+  font-size: 2rem;
+  font-weight: 600;
+  margin-bottom: 1rem;
+`;
+
+const Video = styled.video`
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+`;
+
+const OmedetouText = styled.span`
+  color: white;
+  font-size: 5rem;
+  text-shadow: 2px 2px 2px black;
+  position: absolute;
+  bottom: 4rem;
+  left: 33%;
+`;
+
+const OKButton = styled.button`
+  all: unset;
+  margin-top: 1rem;
+  color: white;
+  background-color: #5890ff;
+  text-align: center;
+  padding: 0.5rem;
+  border-radius: 10px;
+`;
 
 function App() {
+  const [name, setName] = useState<string | null>(null);
+  const [confirm, setConfirm] = useState<boolean>(false);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    setConfirm(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      {confirm ? (
+        <>
+          <OmedetouText>정말 축하해! {name}!</OmedetouText>
+          <Video autoPlay loop>
+            <source src='/omedetou.mp4' type='video/mp4'></source>
+          </Video>
+        </>
+      ) : (
+        <InputContainer>
+          <NameInputBox>
+            <NameQuestionText>What's your name?</NameQuestionText>
+            <input type='text' onChange={handleChange} />
+            <OKButton onClick={handleSubmit}>OK</OKButton>
+          </NameInputBox>
+        </InputContainer>
+      )}
+    </Container>
   );
 }
 
